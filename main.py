9e2680ca15de923a9c8ac58a7e512b6e70e677f3
Rahul <rahulmathur87@gmail.com>
2024-08-30 04:49:41 +0530

    added gravatars

--- main.py
+++ main.py
@@ -44,6 +44,15 @@ class Base(DeclarativeBase):
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
+# configuring Gravatar
+gravatar = Gravatar(app,
+                    size=100,
+                    rating='g',
+                    default='retro',
+                    force_default=False,
+                    force_lower=False,
+                    use_ssl=False,
+                    base_url=None)
 
 
 # CONFIGURE TABLES
